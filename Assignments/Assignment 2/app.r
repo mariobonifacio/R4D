5201d32d950b87bdeca2fb53d7d3978eb8847e24
Mario Bonifacio <mariobonifacio@users.noreply.github.com>
2019-05-20 06:50:19 -0400

    finished_app

--- Assignments/Assignment 2/app.r
+++ Assignments/Assignment 2/app.r
@@ -1,80 +1,37 @@
+library(lubridate); library(tidyverse); library(shiny); library(nycflights13); library(ggthemes)
 
-
-
-function(input, output, session) {
-  
-  # Combine the selected variables into a new data frame
-library(lubridate); library(tidyverse)
-    user_sched_dep <- reactive({
-      hm(paste(input[1], input[2]))
-          
-  })
-  
-    user_sched_dep_lag15 <- if_else(as.numeric(user_sched_dep - minutes(15)) > 0, user_sched_dep - minutes(15), hm('0 00'))
-    user_sched_dep_lead15 <- if_else(as.numeric(user_sched_dep + minutes(15)) < 86400, user_sched_dep + minutes(15), hm('23 59'))
-
-    user_diff_1935h_lag15 <- abs(as.numeric(user_sched_dep_lag15 - hm('19:35')) / 60)
-    user_diff_1935h <- abs(as.numeric(user_sched_dep - hm('19:35')) / 60)   
-    user_diff_1935h_lead15 <- abs(as.numeric(user_sched_dep_lead15 - hm('19:35')) / 60)
-
-    pred_prob_lag15 <- 1/(1+exp(-(0.279603750 - 0.001999519 * (user_diff_1935h - 15))))
-    pred_prob_0 <- 1/(1+exp(-(0.279603750 - 0.001999519 * user_diff_1935h)))
-    pred_prob_lead15 <- 1/(1+exp(-(0.279603750 - 0.001999519 * (user_diff_1935h + 15))))
-  
-
-      
-  output$plot1 <- renderPlot({
-      ggplot() +
-      geom_smooth(aes(x = ))
+ui <- bootstrapPage( ## Define the UI
+  sliderInput(inputId = 'hr', label = 'Scheduled departure hour: ', min = 0, max = 23, value = 12),
+  sliderInput(inputId = 'min', label = 'Scheduled departure minute: ', min = 0, max = 59, value = 30),
+  plotOutput('plot')
+)
 
 library(lubridate); library(tidyverse); library(shiny); library(nycflights13); library(ggthemes)
 
-server <- function(input, output, session) {
-  
-  # Combine the selected variables into a new data frame
-
-  user_sched_dep <- reactive({
-    hm(paste(input[1], input[2]))
-    })
-  
-  user_sched_dep_lag15 <- if_else(as.numeric(user_sched_dep - minutes(15)) > 0, user_sched_dep - minutes(15), hm('0 00'))
-  user_sched_dep_lead15 <- if_else(as.numeric(user_sched_dep + minutes(15)) < 86400, user_sched_dep + minutes(15), hm('23 59'))
-  
-  user_diff_1935h_lag15 <- abs(as.numeric(user_sched_dep_lag15 - hm('19:35')) / 60)
-  user_diff_1935h <- abs(as.numeric(user_sched_dep - hm('19:35')) / 60)   
-  user_diff_1935h_lead15 <- abs(as.numeric(user_sched_dep_lead15 - hm('19:35')) / 60)
-  
-  pred_prob_lag15 <- 1/(1+exp(-(0.279603750 - 0.001999519 * (user_diff_1935h - 15))))
-  pred_prob <- 1/(1+exp(-(0.279603750 - 0.001999519 * user_diff_1935h)))
-  pred_prob_lead15 <- 1/(1+exp(-(0.279603750 - 0.001999519 * (user_diff_1935h + 15))))
-  
-  output$plot1 <- renderPlot({
-    ggplot() +
-      geom_smooth(aes(x = user_diff_1935h, y = pred_prob))
-
-    ggtitle(paste('Your flight has a', pred_prob, 'percent probability of delay')) +
-      theme_fivethirtyeight() +
-      scale_color_fivethirtyeight()
+server <- function(input, output) { ## Define the server code
+    output$plot <- renderPlot({
+    plot(x = c(if_else(input$min >= 15, hm(paste(input$hr, input$min - 15)), hm(paste(input$hr - 1, input$min + 45))),
+               hm(paste(input$hr, input$min)),
+               if_else(input$min <= 44, hm(paste(input$hr, input$min + 15)), hm(paste(input$hr + 1, input$min - 45)))),
+         y = tibble(sched_dep_time_metric = c(if_else(input$min >= 15, input$hr * 100 + (input$min - 15) * 5 / 3, (input$hr - 1) * 100 + (input$min + 45) * 5 /3),
+                                              input$hr * 100 + input$min * 5 / 3,
+                                              if_else(input$min <= 44, input$hr * 100 + (input$min + 15) * 5 / 3, (input$hr + 1) * 100 + (input$min - 45) * 5 /3))) %>%
+           mutate(diff_1935h = abs(sched_dep_time_metric - 1958.33),
+                  pred_prob = as.integer(100/(1+exp(-(0.279603750 - 0.001999519 * diff_1935h))))) %>%
+           pull(pred_prob), 
+         type = 'l',
+         col = 'red',
+         lwd = 3,
+         xlab = 'Scheduled departure time',
+         ylab = 'Percent chance of delay',
+         main = paste('Plan around a chance of delay of ', ))
   })
-  
 }
 
-
-ui <- fluidPage(
-  
-  pageWithSidebar(
-    
-    headerPanel('Probability of delay by scheduled departure time'),
-    sidebarPanel(
-      selectInput('user_hr', 'Scheduled departure hour', 0:24, selected = 12),
-      selectInput('user_min', 'Scheduled departure minute', 0:59, selected = 30)
-  ),
-  mainPanel(
-    plotOutput('plot1')
-  )
-)
-)
+shinyApp(ui = ui, server = server) ## Return a Shiny app object
 
 
-shinyApp(ui = ui, server = server)
 
+#x = c(input$hr * 100 + input$min * 5 / 3,
+#      if(input$min >= 15) input$hr * 100 + (input$min - 15) * 5 / 3 else (input$hr - 1) * 100 + (input$min + 45) * 5 /3,
+#      if(input$min <= 45) input$hr * 100 + (input$min - 15) * 5 / 3 else (input$hr + 1) * 100 + (input$min - 45) * 5 /3)
