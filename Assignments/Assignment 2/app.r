3fcb9c213e0bf65338b0893461eb5506a5de3e2e
Mario Bonifacio <mariobonifacio@users.noreply.github.com>
2019-05-20 17:40:05 -0400

    Standalone shiny app

--- Assignments/Assignment 2/app.r
+++ Assignments/Assignment 2/app.r
@@ -6,30 +6,26 @@ ui <- bootstrapPage( ## Define the UI
   plotOutput('plot')
 )
 
-library(lubridate); library(tidyverse); library(shiny); library(nycflights13); library(ggthemes)
+
 
 server <- function(input, output) { ## Define the server code
     output$plot <- renderPlot({
-    plot(x = c(as.POSIXct(as.character(if_else(input$min >= 15, input$hr * 100 + input$min - 15, (input$hr - 1) * 100 + input$min + 45)), format = '%H%M'),
-               as.POSIXct(as.character(input$hr * 100 +input$min), format = '%H%M'),
-               as.POSIXct(as.character(if_else(input$min <= 44, input$hr * 100 + input$min + 15, (input$hr + 1) * 100 + input$min - 45)), format = '%H%M')),
-         y = tibble(diff_1935h = abs(c(input$hr * 60 + input$min - 15 - 1175, input$hr * 60 + input$min - 1175, input$hr * 60 + input$min + 15 - 1175))) %>%
-                  mutate(pred_prob = 100/(1+exp(-(0.279603750 - 0.001999519 * diff_1935h)))) %>%
-           pull(pred_prob), 
-         type = 'l',
-         col = 'red',
+    plot(x = rep(round(input$hr * 100 +input$min, 1), 3),
+         y = c(100/(1+exp(-(0.279603750 - (0.001999519 - 0.0000148) * abs(input$hr * 60 + input$min - (19 * 60 + 35))))),
+               100/(1+exp(-(0.279603750 - 0.001999519 * abs(input$hr * 60 + input$min - (19 * 60 + 35))))),
+               100/(1+exp(-(0.279603750 - (0.001999519 + 0.0000148) * abs(input$hr * 60 + input$min - (19 * 60 + 35)))))),
+         xlim = range(input$hr * 100 +input$min - 15, input$hr * 100 + input$min + 15),
+         type = 'b',
          lwd = 3,
-         tck = .01,
+         lty = 2,
+         width = 0,
+         tck = .02,
          xlab = 'Scheduled departure time',
          ylab = 'Percent chance of delay',
-         main = paste('Plan around a chance of delay of ', input$hr * 100 + input$min))
+         pch = 3,
+         main = paste('Plan around a chance of delay of ', round(100/(1+exp(-(0.279603750 - (0.001999519) * abs(input$hr * 60 + input$min - (19 * 60 + 35)))))), '%'))
+      
   })
 }
 
 shinyApp(ui = ui, server = server) ## Return a Shiny app object
-
-
-
-#x = c(input$hr * 100 + input$min * 5 / 3,
-#      if(input$min >= 15) input$hr * 100 + (input$min - 15) * 5 / 3 else (input$hr - 1) * 100 + (input$min + 45) * 5 /3,
-#      if(input$min <= 45) input$hr * 100 + (input$min - 15) * 5 / 3 else (input$hr + 1) * 100 + (input$min - 45) * 5 /3)
